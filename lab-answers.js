b8ab24fb60bc98166cf19dc9f73f065c5a30a15a
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-05-26 14:25:38 -0700

    Wild Wild Life answered

--- lab-answers.js
+++ lab-answers.js
@@ -20,17 +20,45 @@
 // Fizz Buzz
 ////////////////////////////////
 
-for (let i = 0; i <= 100; i++) {
-    if(i % 3 === 0){
-        if(i % 5 === 0) {
-            console.log("FizzBuzz");
-        }
-        else{
-            console.log("Fizz");
-        }
-    }
-    else if (i % 5 === 0) {
-        console.log("Buzz");
-    }
-    else console.log(i);
-}
+// for (let i = 0; i <= 100; i++) {
+//     if(i % 3 === 0){
+//         if(i % 5 === 0) {
+//             console.log("FizzBuzz");
+//         }
+//         else{
+//             console.log("Fizz");
+//         }
+//     }
+//     else if (i % 5 === 0) {
+//         console.log("Buzz");
+//     }
+//     else console.log(i);
+// }
+
+////////////////////////////////
+// Wild Wild Life
+////////////////////////////////
+
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+
+wolfy.pop();
+wolfy.push("Gotham City")
+plantee[2] += 1;
+dart.push("Hawkins")
+wolfy.shift();
+wolfy.unshift("Gameboy")
+
+console.log(wolfy);
+console.log(plantee);
+
+////////////////////////////////
+// Yell at the Ninja Turtules
+////////////////////////////////
+
+////////////////////////////////
+// Methods, Revisited
+////////////////////////////////
