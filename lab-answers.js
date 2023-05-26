6ec8c730e7e619079b4d1ee981dab8c20a9f55cd
Ryan Kozin <ryankozin@Ryans-MacBook-Pro.local>
2023-05-26 14:19:14 -0700

    Fizz Buzz answered

--- lab-answers.js
+++ lab-answers.js
@@ -10,12 +10,27 @@
 // Get Even
 ////////////////////////////////
 
-for (let i = 0; i < 200; i++) {
-    if (i%2 === 0) {
-        console.log(i);
-    }
-}
+// for (let i = 0; i < 200; i++) {
+//     if (i%2 === 0) {
+//         console.log(i);
+//     }
+// }
 
 ////////////////////////////////
 // Fizz Buzz
 ////////////////////////////////
+
+for (let i = 0; i <= 100; i++) {
+    if(i % 3 === 0){
+        if(i % 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else{
+            console.log("Fizz");
+        }
+    }
+    else if (i % 5 === 0) {
+        console.log("Buzz");
+    }
+    else console.log(i);
+}
